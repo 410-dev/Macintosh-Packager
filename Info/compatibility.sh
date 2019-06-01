a104f5ee55d36c8f6c9a48aeeb7a2906e7d957d6
Hoyoun Song <hysong@zeone.kro.kr>
2019-06-02 03:08:23 +0900

    5.21 minor bug fixes

--- Info/compatibility.sh
+++ Info/compatibility.sh
@@ -3,7 +3,7 @@ ROOTFS="/"
 LIBRARY=$ROOTFS"usr/local/mpkglib"
 if [[ -e $LIBRARY/db/installed ]]; then
 	echo "Unsupported Database type."
-	echo "Please install mpkg < 2.1 first, then upgrade."
+	echo "Please install mpkg >= 5.2duo first, then upgrade."
 	sudo touch $1/compatibility-error
 	exit
 fi

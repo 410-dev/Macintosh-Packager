75b40cdb0b14c30eff06207da90d46dd07e00e6b
Hoyoun Song <hysong0129@gmail.com>
2019-09-24 17:39:39 +0900

    7.0.2 update

--- Info/postinst.sh
+++ Info/postinst.sh
@@ -7,9 +7,3 @@ if [[ -e $LIBRARY/db/installed ]]; then
 		sudo touch $LIBRARY/convert_failed
 	fi
 fi
-LIBRARY=$LIBRARY/linuxsupport
-if [[ ! -z $(uname -a | grep "Linux") ]]; then
-	sudo cp $LIBRARY/mpkg /usr/local/bin
-	sudo cp $LIBRARY/mpkg-make /usr/local/bin
-	echo "Linux version mpkg installed."
-fi

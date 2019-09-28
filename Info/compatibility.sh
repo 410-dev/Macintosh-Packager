53bd9292d932d0db2912fe7254d6927de95b802c
410 <hysong0129@gmail.com>
2019-09-28 12:12:22 +0900

    Support upgrade

--- Info/compatibility.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ROOTFS="/"
-LIBRARY=$ROOTFS"usr/local/mpkglib"
-if [[ -e $LIBRARY/db/installed ]]; then
-	echo "Unsupported Database type."
-	echo "Please install mpkg >= 5.2duo first, then upgrade."
-	sudo touch $1/compatibility-error
-	exit
-fi
--- Info/postinst.sh
+++ Info/postinst.sh
@@ -2,7 +2,9 @@
 ROOTFS="/"
 LIBRARY=$ROOTFS"usr/local/mpkglib"
 if [[ -e $LIBRARY/db/installed ]]; then
+	echo "Converting depricated library..."
 	sudo rm $LIBRARY/db/installed
+	sleep 3
 	if [[ -e $LIBRARY/db/installed ]]; then
 		sudo touch $LIBRARY/convert_failed
 	fi
